e30aa1143afb110481a06cafd85502709c9b82ba
Jonathan Maldonado Guemarez <cantor.dedekind@gmail.com>
2023-10-07 17:08:02 -0400

    2nd commit: Added the necessary files to showcase the  refactoring the HTML and CSS code provided by Maitree using Handlebars templates, sub templates, and partials.

---
+++ seeds/imageData.js
@@ -0,0 +1,20 @@
+const imageData = [
+  {
+    name: 'Mountains',
+    file_path: '/images/img1.jpg',
+  },
+  {
+    name: 'Hiking',
+    file_path: '/images/img2.jpg',
+  },
+  {
+    name: 'Canoying',
+    file_path: '/images/img3.jpg',
+  },
+  {
+    name: 'Activities',
+    file_path: '/images/img4.jpg',
+  },
+];
+
+module.exports = imageData;
